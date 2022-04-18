12b4bd409e1454ee15e023a44a8c0f2943a5b12f
aagraston <aagraston@gmail.com>
2022-04-18 11:22:27 -0700

    routing functional

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ const url = require('url')
 
 const port = process.env.PORT || 3000
 
+let page404 = fs.readFileSync('404.html', (err, data) => {
+  if (err) throw err
+  return data
+})
+
 const server = http.createServer((req, res) => {
   
   //getting the filename from the url
   let q = url.parse(req.url, true)
   let fileName = `.${q.pathname}`
   if (fileName === './') {
-    filename = './index.html'
+    fileName = './index.html'
   }
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      res.write(page404)
+      return res.end()
+    }
+
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write(data)
+    return res.end()
+  })
 
 
 })
@@ -21,5 +37,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`server running at port ${port}`)
 })
-
-const homeURL = new URL('/', 'https://basicsite.com/')
